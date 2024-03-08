b1cf30caaa9e3fa0d79ae4b9cbd1219c585cacbd
Adam Olsen <olsen.adam06@gmail.com>
2024-03-07 17:30:58 -0700

    contains pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -53,4 +53,8 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
+    public boolean contains(City city) {
+        return cities.contains(city);
+    }
+
 }
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -28,6 +29,14 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    @Test
+    public void hasCity() {
+        list = MockCityList();
+        City aCity = new City("Estevan", "SK");
+        list.addCity(aCity);
+        assertTrue(list.contains(aCity));
     }
 
 }
